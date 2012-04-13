aa70fe99d48814b8e4c313319e3bdc6f7d6925a7
Jeff Hansen <dscheffy@gmail.com>
2012-04-13 18:21:28 -0500

    Initial commit with one class and one test.

---
+++ src/main/java/org/projectsnailtrail/writable/TrackPoint.java
@@ -0,0 +1,165 @@
+package org.projectsnailtrail.writable;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class TrackPoint {
+
+	public double getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(double latitude) {
+		hasLatitude=true;
+		this.latitude = latitude;
+	}
+	public double getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(double longitude) {
+		hasLongitude=true;
+		this.longitude = longitude;
+	}
+	public long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(long timestamp) {
+		hasTimestamp=true;
+		this.timestamp = timestamp;
+	}
+	public int getAccuracy() {
+		return accuracy;
+	}
+	public void setAccuracy(int accuracy) {
+		hasAccuracy=true;
+		this.accuracy = accuracy;
+	}
+	public boolean hasLatitude() {
+		return hasLatitude;
+	}
+	public boolean hasLongitude() {
+		return hasLongitude;
+	}
+	public boolean hasTimestamp() {
+		return hasTimestamp;
+	}
+	public boolean hasAccuracy() {
+		return hasAccuracy;
+	}
+	private double latitude;
+	private double longitude;
+	private long timestamp;
+	private int accuracy;
+	private boolean hasLatitude;
+	private boolean hasLongitude;
+	private boolean hasTimestamp;
+	private boolean hasAccuracy;
+	private final static byte VERSION = 1;
+	
+	public void write(OutputStream os) throws IOException {
+		//one day I'll make this more efficient, for  now it'll just live as V1
+		//really the booleans only need a bit each instead of a byte, four bits would suffice for accuracy if I make it a logrythmic and version could probably be limited if need be
+		DataOutputStream dos = new DataOutputStream(os);
+		dos.writeByte(VERSION);
+		dos.writeBoolean(hasAccuracy);
+		dos.writeBoolean(hasLatitude);
+		dos.writeBoolean(hasLongitude);
+		dos.writeBoolean(hasTimestamp);
+		if(hasAccuracy) dos.writeInt(accuracy);
+		if(hasLatitude) dos.writeInt(convertDoubleToInt(latitude));
+		if(hasLongitude) dos.writeInt(convertDoubleToInt(longitude));
+		if(hasTimestamp) dos.writeLong(timestamp);
+		dos.flush(); //hmm, is it being a good citizen to do this here?
+	}
+	public void read(InputStream is) throws IOException {
+		DataInputStream dis = new DataInputStream(is);
+		byte version = dis.readByte();
+		if(version>VERSION) throw new IOException("Input contains a version of serialization that is higher than the current one supported: " + version + ">" + VERSION);
+		if(version==1){
+			hasAccuracy = dis.readBoolean();
+			hasLatitude = dis.readBoolean();
+			hasLongitude = dis.readBoolean();
+			hasTimestamp = dis.readBoolean();
+			if(hasAccuracy) accuracy = dis.readInt();
+			if(hasLatitude) latitude = convertIntToDouble(dis.readInt());
+			if(hasLongitude) longitude = convertIntToDouble(dis.readInt());
+			if(hasTimestamp) timestamp = dis.readLong();
+		}
+		
+	}
+	public String toString(){
+		return null;
+	}
+	
+	public static int convertDoubleToInt(double d){
+		if(d==180){
+			d=-180;
+		}
+		if(d>180||d<-180){
+			//need to handle this with some kind of out of bounds exception or pass back something that makes sense. 
+			//not sure 270 would validly be convertable to -90...
+			throw new RuntimeException("Value is out of bounds -- not a valid latitude or longitude: "+String.valueOf(d));
+		}
+		return (int)((-d / 180) * Integer.MIN_VALUE);
+
+	}
+	public static double convertIntToDouble(int i){
+		//Don't need to worry about out of bounds situations here because every int maps to a specific double
+		//well, that's not entirely true -- latitudes above 90 or below -90 aren't legitimate, but this is
+		//a generic converter that handles longitudes as well.
+		return -(((double)i)/Integer.MIN_VALUE * 180);
+		
+	}
+	public static Iterable<TrackPoint> iterate(final InputStream is){
+		return new Iterable<TrackPoint>(){
+
+			@Override
+			public Iterator<TrackPoint> iterator() {
+				
+				return new Iterator<TrackPoint>() {
+					boolean more;
+					TrackPoint next;
+
+					@Override
+					public boolean hasNext() {
+						if(!more) {
+							next = new TrackPoint();
+							try{
+								next.read(is);
+								more = true;
+							}catch(IOException ioe){
+								more =false;
+								next =null;
+								//assume we're done with the stream (although i suppose other problems could cause this)
+								//I realize this is an expensive way to signal we're done reading the stream -- may refactor later on
+							}
+						}
+						return more;
+					}
+
+					@Override
+					public TrackPoint next() {
+						if(!more){
+							if(!hasNext()) throw new NoSuchElementException();
+						}
+						more=false;
+						return next;
+					}
+
+					@Override
+					public void remove() {
+						throw new UnsupportedOperationException("remove is not supported by TrackPoint iterator");
+						
+					}
+					
+				};
+			}
+			
+		};
+		
+	}
+}
---
+++ src/test/java/org/projectsnailtrail/writable/TrackPointTest.java
@@ -0,0 +1,110 @@
+package org.projectsnailtrail.writable;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+
+import org.junit.Test;
+
+public class TrackPointTest {
+
+	@Test
+	public void testSerialization() throws Exception {
+		TrackPoint tp = new TrackPoint();
+		tp.setLatitude(43.21);
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		tp.write(baos);
+		TrackPoint tp1 = new TrackPoint();
+		tp1.read(new ByteArrayInputStream(baos.toByteArray()));
+		assertTrue("tp1 should have a latitude", tp1.hasLatitude());
+		assertFalse("tp1 shouldn't have a longitude", tp1.hasLongitude());
+		assertEquals("tp and tp1 should have the same latitude", tp.getLatitude(), tp1.getLatitude(), .000001);
+		
+		tp.setLongitude(-83.2341);
+		tp.setTimestamp(234827938);
+		tp.setAccuracy(20);
+		baos.reset();
+		tp.write(baos);
+		tp1.read(new ByteArrayInputStream(baos.toByteArray()));
+		assertTrue("tp1 should have a latitude", tp1.hasLatitude());
+		assertTrue("tp1 should have a longitude", tp1.hasLongitude());
+		assertTrue("tp1 should have accuracy", tp1.hasAccuracy());
+		assertTrue("tp1 should have a timestamp", tp1.hasTimestamp());
+		assertEquals("tp and tp1 should have the same latitude", tp.getLatitude(), tp1.getLatitude(), .000001);
+		assertEquals("tp and tp1 should have the same longitude", tp.getLongitude(), tp1.getLongitude(), .000001);
+		assertEquals("tp and tp1 should have the same accuracy", tp.getAccuracy(), tp1.getAccuracy(), .000001);
+		assertEquals("tp and tp1 should have the same timestamp", tp.getTimestamp(), tp1.getTimestamp(), .000001);
+		
+		// create an input stream with four track points -- tp, then tp2, then tp, then tp2 again (repeating because I'm lazy)
+		TrackPoint tp2 = new TrackPoint();
+		tp2.setLongitude(141.798355);
+		tp2.setLatitude(-64.837443);
+		tp2.setAccuracy(50);
+		tp2.setTimestamp(123456789);
+		
+		baos.reset();
+		tp.write(baos);
+		tp2.write(baos);
+		tp.write(baos);
+		tp2.write(baos);
+		
+		int counter = 0;
+		for(TrackPoint tpx : TrackPoint.iterate(new ByteArrayInputStream(baos.toByteArray()))){
+			counter +=1;
+			if(counter % 2==1){
+				assertTrue("tp# "+counter+" should have a latitude", tpx.hasLatitude());
+				assertTrue("tp# "+counter+" should have a longitude", tpx.hasLongitude());
+				assertTrue("tp# "+counter+" should have accuracy", tpx.hasAccuracy());
+				assertTrue("tp# "+counter+" should have a timestamp", tpx.hasTimestamp());
+				assertEquals("tp# "+counter+" and tp should have the same latitude", tpx.getLatitude(), tp.getLatitude(), .000001);
+				assertEquals("tp# "+counter+" and tp should have the same longitude", tpx.getLongitude(), tp.getLongitude(), .000001);
+				assertEquals("tp# "+counter+" and tp should have the same accuracy", tpx.getAccuracy(), tp.getAccuracy(), .000001);
+				assertEquals("tp# "+counter+" and tp should have the same timestamp", tpx.getTimestamp(), tp.getTimestamp(), .000001);
+				
+			} else {
+				assertTrue("tp# "+counter+" should have a latitude", tpx.hasLatitude());
+				assertTrue("tp# "+counter+" should have a longitude", tpx.hasLongitude());
+				assertTrue("tp# "+counter+" should have accuracy", tpx.hasAccuracy());
+				assertTrue("tp# "+counter+" should have a timestamp", tpx.hasTimestamp());
+				assertEquals("tp# "+counter+" and tp2 should have the same latitude", tpx.getLatitude(), tp2.getLatitude(), .000001);
+				assertEquals("tp# "+counter+" and tp2 should have the same longitude", tpx.getLongitude(), tp2.getLongitude(), .000001);
+				assertEquals("tp# "+counter+" and tp2 should have the same accuracy", tpx.getAccuracy(), tp2.getAccuracy(), .000001);
+				assertEquals("tp# "+counter+" and tp2 should have the same timestamp", tpx.getTimestamp(), tp2.getTimestamp(), .000001);
+
+			}
+			
+		}
+		assertEquals("There should have been 4 track points in the iterator", 4, counter);
+		
+		
+	}
+	
+	@Test
+	public void testConversion() {
+		assertEquals("Zero should convert to zero", 0,TrackPoint.convertDoubleToInt(0));
+		//Signed ints make this a bit confusing -- not sure if I should bother to flip the sign. 
+		//This makes more sense if you think of it as an unsigned int though.
+		assertEquals("180 should convert to min int", Integer.MIN_VALUE, TrackPoint.convertDoubleToInt(180));
+		assertEquals("90 should convert to one half of max int", -(Integer.MIN_VALUE/2), TrackPoint.convertDoubleToInt(90));
+		assertEquals("45 should convert to one half of max int", -(Integer.MIN_VALUE/4), TrackPoint.convertDoubleToInt(45));
+		//This is actually kind of tricky -- in reality 180 and -180 represent the same exact point, 
+		//so they should both convert to the same point.  
+		assertEquals("-180 should convert to min int", Integer.MIN_VALUE, TrackPoint.convertDoubleToInt(-180));
+		//being lazy and referring to min/max Integer doesn't work so well because the min value is equal to a 
+		//power of two whereas the max value is one less than a power of two.  
+		assertEquals("-90 should convert to one half of min int", Integer.MIN_VALUE/2, TrackPoint.convertDoubleToInt(-90));
+		assertEquals("-45 should convert to one half of min int", Integer.MIN_VALUE/4, TrackPoint.convertDoubleToInt(-45));
+		
+		//Now lets test that the conversion is reversible
+		//At it's greatest, a microdegree is about a meter, so 1/10th of a microdegree (.0000001) is one tenth of a meter
+		//These tests confirm that the back and forth conversion is accurate to a tenth of a meter (on the tests given).
+		double tenthOfMicroDegree = .0000001;
+		assertEquals("90 should convert back to 90", 90, TrackPoint.convertIntToDouble(TrackPoint.convertDoubleToInt(90)), tenthOfMicroDegree);
+		assertEquals("60 should convert back to 60", 60, TrackPoint.convertIntToDouble(TrackPoint.convertDoubleToInt(60)), tenthOfMicroDegree);
+		assertEquals("25.3293 should convert back to 25.3293", 25.3293, TrackPoint.convertIntToDouble(TrackPoint.convertDoubleToInt(25.3293)), tenthOfMicroDegree);
+		assertEquals("-10 should convert back to -10", -10, TrackPoint.convertIntToDouble(TrackPoint.convertDoubleToInt(-10)), tenthOfMicroDegree);
+	}
+	
+
+}
